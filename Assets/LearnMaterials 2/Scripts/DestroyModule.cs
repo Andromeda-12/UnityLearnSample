0ab94ca94ef7a8c9e6c168ab3cac816e15b3c05b
Andromeda-12 <55835078+Andromeda-12@users.noreply.github.com>
2022-02-14 23:31:57 +0700

    Сделал первую часть 5 лабы

--- Assets/LearnMaterials 2/Scripts/DestroyModule.cs
+++ Assets/LearnMaterials 2/Scripts/DestroyModule.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 [HelpURL("https://docs.google.com/document/d/1RMamVxE-yUpSfsPD_dEa4-Ak1qu6NTo83qY1O4XLxUY/edit?usp=sharing")]
 public class DestroyModule : MonoBehaviour
 {
+    [SerializeField]
+    [Min(0)]
     private float destroyDelay;
+
+    [SerializeField]
+    [Range(0, 294)]
     private int minimalDestroyingObjectsCount;
 
     private Transform myTransform;
+
 
     private void Awake()
     {
         myTransform = transform;
     }
 
+    [ContextMenu("Sart")]
     public void ActivateModule()
     {
         StartCoroutine(DestroyRandomChildObjectCoroutine());
