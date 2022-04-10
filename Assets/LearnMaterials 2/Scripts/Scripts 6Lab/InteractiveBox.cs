8b0b64b4f9c71bcddfc8af665b1c1d27c7d30393
Andromeda-12 <55835078+Andromeda-12@users.noreply.github.com>
2022-04-10 19:23:37 +0700

    Доделал лабораторную

--- Assets/LearnMaterials 2/Scripts/Scripts 6Lab/InteractiveBox.cs
+++ Assets/LearnMaterials 2/Scripts/Scripts 6Lab/InteractiveBox.cs
@@ -10,16 +10,22 @@ public class InteractiveBox : MonoBehaviour
     public void AddNext(InteractiveBox box)
     {
         if (box != null)
+        {
             next = box;
+            next.GetComponent<ObstacleItem>()?.onDestroyObstacle.AddListener(() => next = null);
+        }
     }
+
     private void FixedUpdate()
     {
         if (next != null && Physics.Linecast(transform.position, next.transform.position, out RaycastHit hit))
         {
             Debug.DrawLine(transform.position, hit.point, Color.red);
-            if (next.GetComponent<ObstacleItem>() != null)
-                next.GetComponent<ObstacleItem>().GetDamage(Time.deltaTime);
-        }
 
+            if (next.GetComponent<ObstacleItem>() != null)
+            {
+                next.GetComponent<ObstacleItem>().GetDamage(Time.deltaTime);
+            }
+        }
     }
 }
